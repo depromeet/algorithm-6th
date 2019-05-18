2768e88fd29a45dbb92582b8cb6206179c70e98c
zion830 <zion830@naver.com>
2019-05-19 00:54:58 +0900

    1차 스터디 파일 정리

--- team1/BF/jihun/Solution04.py
+++
@@ -1,7 +0,0 @@
-def solution(brown, red):
-    lec = brown + red
-    for r in range(1, red // 2 + 2):
-        if red % r == 0:
-            for b in range(r + 2, brown // 2 + 2):
-                if lec % b == 0 and lec // b > red // r + 1:
-                    return [lec // b, b]
--- team1/Search/dongkun/search-4.py
+++
@@ -1,5 +0,0 @@
-# 주어진 숫자의 부분집합 각각의 Permutation을 모두 소수 검사를 하면 될 것 같습니다.
-# 하지만 파이썬에서 제공하는 permutation 함수를 쓰지 않고 구현하려니 어려움이 있습니다.
-def solution(numbers):
-    answer = 0
-    return answer
