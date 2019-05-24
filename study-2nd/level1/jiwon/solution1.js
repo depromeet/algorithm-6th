fdd5720d947cb6366fc7477925953c15c92c998a
leejiwonn <37164467+leejiwonn@users.noreply.github.com>
2019-05-24 13:40:18 +0900

    죄송합니다 ㅠㅠㅠ

--- study-2nd/level1/jiwon/solution1.js
+++
@@ -1,9 +0,0 @@
-function solution(s){
-    if(s.match(/p/gi).length == s.match(/y/gi).length){
-        return true;
-    }else if(s.match(/p/gi).length && s.match(/y/gi).length == 0){
-        return true;
-    }else{
-        return false;
-    }
-}
