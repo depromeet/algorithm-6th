2af025a3859c8c5e1d75fa60d614cc688073b9c5
yongseongjeon <yongseong.jeon95@gmail.com>
2019-05-25 14:12:05 +0900

    [level1] 1번, 2번 수정

--- study-2nd/level1/yongseong/Solution01.js
+++ study-2nd/level1/yongseong/Solution01.js
@@ -1,9 +1,9 @@
-function solution(phone_number) {
+function solution(phoneNumber) {
     // 전화번호 뒷 4자리를 추출합니다.
-    var backFourNumber = phone_number.slice(phone_number.length - 4);
+    const backFourNumber = phone_number.slice(phone_number.length - 4);
     // 전화번호 길이만큼 *를 생성합니다.
     var star = '';
-    for ( var i = 0; i < phone_number.length - 4; i++ ) {
+    for ( var i = 0; i < phoneNumber.length - 4; i++ ) {
         star += '*';   
     }
     // *와 뒷 4자리 더한 값을 리턴합니다.
--- study-2nd/level1/yongseong/Solution02.js
+++ study-2nd/level1/yongseong/Solution02.js
@@ -1,13 +1,13 @@
 function solution(x, n) {
-    var array = [];
+    var numbers = [];
     const plusNumber = x;
     // n만큼 반복합니다.
     for ( var i = 0; i < n; i++ ) {
         // x를 배열에 삽입합니다.
-        array.push(x);
+        numbers.push(x);
         // x를 증가시킵니다.
         x += plusNumber;
     }
     // 배열을 리턴합니다.
-    return array;
+    return numbers;
 }
